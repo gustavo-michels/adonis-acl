d8a5ac5d7f5e9c32d883dee7da0e075f2993d84d
Gustavo Henrique Michels <ghmichels@gmail.com>
2019-07-27 19:34:57 -0300

    att

--- start/routes.js
+++ start/routes.js
@@ -17,9 +17,29 @@ const Route = use('Route')
 
 Route.post('/sessions', 'SessionController.store')
 
+Route.get('/users', 'UserController.index')
 Route.post('/users', 'UserController.store')
 Route.put('/users/:id', 'UserController.update').middleware('auth')
 
 Route.resource('/posts', 'PostController')
+  .apiOnly()
+  .except(['index', 'show'])
+  .middleware(['auth', 'is:(administrator || moderator)'])
+
+Route.get('/posts', 'PostController.index').middleware([
+  'auth',
+  'can:(read_posts ||read_private_posts)'
+])
+
+Route.get('/posts/:id', 'PostController.show').middleware([
+  'auth',
+  'can:(read_posts ||read_private_posts)'
+])
+
+Route.resource('permissions', 'PermissionController')
+  .apiOnly()
+  .middleware('auth')
+
+Route.resource('roles', 'RoleController')
   .apiOnly()
   .middleware('auth')
